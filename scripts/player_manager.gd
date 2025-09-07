e4747a5b626bb7bb5e38261dde6d485e428172c3
KJ <kjk@skyhopper.net>
2025-09-07 12:30:38 -0400

    added comments clarity & todo's

--- scripts/player_manager.gd
+++ scripts/player_manager.gd
@@ -4,7 +4,7 @@ var current_char: CharacterBody3D
 
 func _ready() -> void:
 	# TODO: Should load or somehow maintain the character's selected char throughout scenes
-	# e.g. if the player leaves scene1 and enters scene 2, they should have the same selected character
+	# e.g. if the player leaves scene1 and enters scene2, they should have the same selected character
 	current_char = get_child(0) # temp
 	current_char.set_process_mode(Node.PROCESS_MODE_ALWAYS)
 	for char in get_children():
@@ -21,12 +21,28 @@ func _process(delta: float) -> void:
 	elif (Input.is_action_just_pressed("select_char3")):
 		change_char(2)
 
+'''
+	Uses node hierarchy for character switching, assuming characters are the only direct children 
+	under the player manager. If not, could just group characters under another child node
+	e.g. get_node("characters").get_child(idx)
+	- PlayerManager
+	- - Characters
+	- - - Player1
+	- - - Player2
+		  ...
+'''
 func change_char(idx: int):
 	if idx < get_child_count() and current_char.name != get_child(idx).name:
-			current_char.visible = false
-			current_char.set_process_mode(Node.PROCESS_MODE_DISABLED)
-			get_child(idx).set_global_transform(current_char.get_global_transform())
-			
-			current_char = get_child(idx)
-			current_char.visible = true
-			current_char.set_process_mode(Node.PROCESS_MODE_ALWAYS)
+		var new_char: CharacterBody3D = get_child(idx)
+		
+		current_char.visible = false
+		current_char.set_process_mode(Node.PROCESS_MODE_DISABLED)
+		new_char.set_global_transform(current_char.get_global_transform())
+		
+		# TODO: Manage state
+		# Could set the new character's state to the former selected character's through some sort of map
+		# Could also just reset the state to some normal one like idle or walk if the FSM is good enough
+		
+		current_char = new_char
+		current_char.visible = true
+		current_char.set_process_mode(Node.PROCESS_MODE_ALWAYS)
