e3a3476418e340ceaee5f596ebcf1455b1dd5a66
Arshiya Rahman <127697399+arahman302@users.noreply.github.com>
2025-09-07 20:51:29 -0400

    potentially working shader?

---
+++ scenes/arshiya_r/scripts/hit_trigger.gd
@@ -0,0 +1,18 @@
+extends Node
+
+@onready var main: GPUParticles3D = $HIT/main
+@onready var sparks: GPUParticles3D = $HIT/sparks
+
+var enabled: bool = false
+var timeElapsed: float = 0
+@export var timeLimit: float = 0.5
+
+func _ready():
+	main.emitting = true
+	print("started the hit!")
+	
+func _physics_process(delta):
+	timeElapsed += delta
+	if (!enabled && timeElapsed >= timeLimit):
+		print("started the sparks!")
+		sparks.emitting = true
