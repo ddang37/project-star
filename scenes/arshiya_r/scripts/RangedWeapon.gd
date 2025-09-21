2f111f02c47d7e2cc07acd30ddebd01b484e3b34
Arshiya Rahman <127697399+arahman302@users.noreply.github.com>
2025-09-21 14:05:47 -0400

    ITS WORKING NOW DON'T BREAK IT PLS

--- scenes/arshiya_r/scripts/RangedWeapon.gd
+++ scenes/arshiya_r/scripts/RangedWeapon.gd
@@ -1,10 +1,25 @@
 class_name RangedWeapon extends Node3D
 
 @export var speed:= 10
-@export var weaponLifetime := 3.0
-
 var direction := Vector3.ZERO
 
 @onready var timer: Timer = $Timer
 @onready var hitbox: Hitbox = $Hitbox
-@onready var impact_detector: Hitbox = $ImpactDetector
+
+func _ready():
+	#set_as_top_level(true)
+	look_at(direction)
+
+	
+func _physics_process(delta: float) -> void:
+	position += direction * speed * delta
+
+func _on_timer_timeout() -> void:
+	print("removing bullet")
+	queue_free()
+
+func _on_hitbox_body_entered(body: Node3D) -> void:
+	if (body.name.substr(0, 7) == "Player"):
+		print("collsion detected")
+		#do damage here
+		queue_free()
