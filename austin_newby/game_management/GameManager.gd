db6b983938bec99414793edb5a5bff73b94c06d0
DiscountGrocery <austinmnewby@gmail.com>
2025-09-26 14:57:11 -0400

    Levels + Loading Screen
    I connected Christian's awesome loading screen to the game manager and added two dummy levels.

--- austin_newby/game_management/GameManager.gd
+++ austin_newby/game_management/GameManager.gd
@@ -1,10 +1,9 @@
 extends Node
 
-const LEVEL_PATH := "res://scenes/%s.tscn"
-var player_manager : Node3D:
-	set(a):
-		player_manager = a
-		print_rich("[color=dark_gray] - PlayerManager set.")
+const LEVEL_PATH := "res://austin_newby/levels/%s.tscn"
+const LOADING_SCREEN = preload("uid://c41j5ds1pfvyx")
+
+var player_manager : Node3D
 var player_pos : Vector3:
 	get: return player_manager.position
 var current_level
@@ -13,20 +12,16 @@ var main_scene : MainScene
 func _init() -> void:
 	print_rich("[color=dark_gray]GameManager initalized.")
 
-
+# unloads current level and calls loading functions inside of loading_scene
 func load_level(level_name: String):
 	unload_level()
-	var path := LEVEL_PATH % level_name
-	var new_level := load(path)
-	if new_level:
-		current_level = new_level.instantiate()
-		main_scene.add_child(current_level)
-		print_rich("[color=dark_gray]Loaded new level: %s" % [level_name])
+	var loading_scene = LOADING_SCREEN.instantiate()
+	main_scene.add_child(loading_scene)
+	loading_scene.change_scene(LEVEL_PATH % level_name)
 
-
+# destroys current level
 func unload_level():
 	if (is_instance_valid(current_level)):
-		print_rich("[color=dark_gray]Unloaded level: %s" % [current_level.name])
 		current_level.queue_free()
-		main_scene.get_child(0).queue_free()
+		print_rich("[color=dark_gray]Unloaded level: %s" % [LEVEL_PATH % current_level.name])
 	current_level = null
--- austin_newby/game_management/main_scene.gd
+++ austin_newby/game_management/main_scene.gd
@@ -7,8 +7,9 @@ This node serves as the highest node in the tree.
 
 All levels/menus are added as children of this node.
 There is only ever one level loaded at once.
+Loading is handled by loading_screen.gd and is called in GameManager.gd
 """
 
 func _ready() -> void:
 	GameManager.main_scene = self
-	GameManager.load_level("COPY_THIS")
+	GameManager.load_level("Copied_Level1")
